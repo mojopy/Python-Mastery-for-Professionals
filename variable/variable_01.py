fdc17463337cd5230844c030603912db05ad2035
mojopy <mp@mojopy.dev>
2024-05-13 19:44:25 +0600

    variable day 1

--- variable/variable_01.py
+++ variable/variable_01.py
@@ -1,3 +1,18 @@
 age = 7      # age is of type int
-name = "Karim" # name is now of type str
+name = "The Python cheat sheet is a one-page reference sheet for the Python 3 programming language." # name is now of type str
 print(name)
+
+
+a = 7 # Int/Integer
+b = 8
+c = "sabuj"
+d = 7.00
+
+a = str(a)
+"""###
+This is a comment
+atls todoo
+aldk 
+###"""
+print(a + c)
+
