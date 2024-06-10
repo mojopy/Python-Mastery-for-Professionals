c537a66ebabe95d93c28a2d2dfef9fe55e7795f8
mohammadali202356 <mohammadali202356@gmail.com>
2024-06-10 19:52:05 +0600

    Add files via upload

---
+++ orio.py
@@ -0,0 +1,65 @@
+name = "orio"
+age = 17
+height = 5.6
+result = "name: " +name + ",age:" + str(age) + ",height:" +str(height) 
+print(result)
+
+
+
+about = "orio"
+about2 = 25
+about3 =5.5
+result= "name: " +about + ",age: " +str(about2) + ",height: " +str(about3)
+print(result)
+
+
+
+
+aim1 = "coding"
+aim2 = 6.0
+aim3 = 20
+result = "Aim1:%s, Aim2:%d, Aim3:%.1f" % (aim1, aim2, aim3)
+print(result)
+
+
+
+Data1 = "khalid"
+Data2 = "nirob"
+age1 = 17
+age2 = 20
+height1 = 5.5
+height2 = 6.3
+
+result1 = "There are two friends , Name " +Data1 + " and " +Data2 + ".They are " +str(age1) + " and " + str(age2) + " years old."
+"Their height is " + str(height1) + "and " + str(height2)
+
+print(result1)
+
+
+name = "orio"
+age = 17
+height = 6.76333
+result = "Name:%s, Age:%d, Height:%.2f" % (name,age,height) 
+print(result)
+
+
+
+name1 = "Mohammad"
+age1 = 18
+speed = 31.3465
+result = "Name: %s, Age: %d, Running speed: %.2f" % (name1,age1,speed)
+print(result)
+
+
+
+#Boolean type data
+Mark1 = 100
+Mark2 = 80
+print (Mark1 > Mark2)
+
+
+Num = 2.02
+num = 2
+print (Num == num)
+
+
