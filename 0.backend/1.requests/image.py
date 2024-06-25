fcc2d301a7cd09eae30023b3637716dd8d74e3b4
mojopy <mp@mojopy.dev>
2024-06-25 09:42:05 +0600

    requests updated.

--- 0.backend/1.requests/image.py
+++ 0.backend/1.requests/image.py
@@ -1,5 +1,5 @@
 import requests
-r = requests.get("https://en.wikipedia.org/wiki/File:Cat_August_2010-4.jpg")
+r = requests.get("https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg")
 
 with open("cat.jpg", "wb") as f:
     f.write(r.content)
