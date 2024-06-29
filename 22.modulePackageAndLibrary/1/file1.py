05da80344ac4672d4c214594ae43025e649549e0
mojopy <mp@mojopy.dev>
2024-06-29 19:36:27 +0600

    moudle updated

---
+++ 22.modulePackageAndLibrary/1/file1.py
@@ -0,0 +1,5 @@
+names = {"name1":"Rahim", "name2":"Karim"}
+
+def print_names():
+    print(names["name1"])
+    print(names["name2"])
---
+++ 22.modulePackageAndLibrary/1/file2.py
@@ -0,0 +1,5 @@
+import file1
+
+print(file1.names["name1"])
+
+file1.print_names()
