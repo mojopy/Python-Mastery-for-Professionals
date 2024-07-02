bc8cbdb2d62c3059f25083bb0bd96004fb58eb7f
mojopy <mp@mojopy.dev>
2024-07-02 20:07:47 +0600

    requirements added.

--- 0.1.backend/5.fastapi/examples/pathParameters/ppitems.py
+++ 0.1.backend/5.fastapi/examples/pathParameters/ppitems.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from enum import Enum
 
 router = APIRouter(
     prefix="/ppitems",
@@ -12,3 +13,27 @@ async def read_item(item_id):
 @router.get("/int/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+
+
+#Working with Enum
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@router.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+#Path parameters containing paths
+@router.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
