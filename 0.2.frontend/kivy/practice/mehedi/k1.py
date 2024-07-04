00e42bfa966e48882e0a22de67dfb93ceb3e378b
mehedihasansuchee <mehedihasansuchee@outlook.com>
2024-07-04 17:22:02 +0600

    kivy

---
+++ 0.2.frontend/kivy/practice/mehedi/k1.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello, Kivy!')
+
+if __name__ == '__main__':
+    MyApp().run()
