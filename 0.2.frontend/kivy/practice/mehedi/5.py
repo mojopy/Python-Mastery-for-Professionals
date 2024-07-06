f06e93bfa989996af77d1246562add7ac2240484
mehedihasansuchee <mehedihasansuchee@outlook.com>
2024-07-06 15:50:55 +0600

    kivy

---
+++ 0.2.frontend/kivy/practice/mehedi/5.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
+        btn1 = Button(text='Button 1', size_hint=(1, 0.2))
+        btn2 = Button(text='Button 2', size_hint=(1, 0.5))
+        btn3 = Button(text='Button 3', size_hint=(1, 1))
+
+        layout.add_widget(btn1)
+        layout.add_widget(btn2)
+        layout.add_widget(btn3)
+
+        return layout
+
+if __name__ == '__main__':
+    MyApp().run()
