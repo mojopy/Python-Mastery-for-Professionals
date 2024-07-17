356f3e0dccc809d377170529206ce96ff951ff7c
mojopy <mp@mojopy.dev>
2024-07-17 09:05:02 +0600

    front end organized

--- 0.2.frontend/kivy/practice/mehedi/PageLayout/10.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.pagelayout import PageLayout
-
-class MyApp(App):
-    def build(self):
-        return PageLayout()
-
-if __name__ == '__main__':
-    MyApp().run()
